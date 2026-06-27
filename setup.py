b66d139a061c4e9902847c323e3c80033c229f44
Adarsh208501 <adarshsharmadbg01@gmail.com>
2026-06-27 14:44:06 +0530

    first set up

---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import find_packages, setup
+from typing import List
+
+
+def get_requirements(file_path: str) -> List[str]:
+    """
+    Returns list of requirements
+    """
+
+    requirements = []
+
+    with open(file_path) as file_obj:
+        requirements = file_obj.readlines()
+        requirements = [req.replace("\n", "") for req in requirements]
+
+    return requirements
+
+
+setup(
+    name="MLproject",
+    version="0.0.1",
+    author="Adarsh",
+    author_email="adarshsharma.iitkgp@gmail.com",
+    packages=find_packages(),
+    install_requires=get_requirements("requirements.txt"),
+)
